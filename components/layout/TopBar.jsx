bc83353c89786eae18066e1ec574b8739623dc15
Yogesh Navghane <124075039+yogeshNavghane67@users.noreply.github.com>
2024-02-02 17:15:51 +0530

    Topbar

--- components/layout/TopBar.jsx
+++ components/layout/TopBar.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+"use client";
+
+import { SignOutButton, SignedIn } from "@clerk/nextjs";
+import { Add, Logout, Search } from "@mui/icons-material";
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
 
 const TopBar = () => {
+  const router = useRouter();
+  const [search, setSearch] = useState("");
   return (
-    <div>TopBar</div>
-  )
-}
+    <div className="flex justify-between items-center mt-6">
+      <div className="relative">
+        <input
+          type="text"
+          className="search-bar"
+          placeholder="Search posts, people, ..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <Search className="search-icon" onClick={() => {}} />
+      </div>
+      <button
+        className="create-post-btn"
+        onClick={() => router.push("/create-post")}
+      >
+        <Add />
+        <p>Create A Post</p>
+      </button>
 
-export default TopBar
+      <div className="flex gap-3">
+      <SignedIn>
+        <SignOutButton>
+          <div className='flex  cursor-pointer items-center md:hidden'>
+          <Logout sx={{ color: "white", fontSize:'32px'}}/>
+          </div>
+        </SignOutButton>
+      </SignedIn>
+
+      <Link href="/">
+        <Image src="/assets/phucmai.png" alt="profile photo" width={50} height={50} className="rounded-full md:hidden" />
+      </Link>
+      </div>
+
+      
+    </div>
+  );
+};
+
+export default TopBar;
